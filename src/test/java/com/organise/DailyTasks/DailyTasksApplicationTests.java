50edc9daec4405cc57e698699209a23b38b12d9c
Thomas Soldwedel <thomas.soldwedel@fdmgroup.com>
2024-01-22 13:42:02 +0100

    initial commit

---
+++ src/test/java/com/organise/DailyTasks/DailyTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.organise.DailyTasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyTasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
